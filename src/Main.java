9797882c5fb40b24a1b6befb3257b300ced78571
Jakub Orzyłowski <s21001@pjwstk.edu.pl>
2021-10-31 00:25:34 +0200

    Zabawy z localDatetime

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,74 @@
-import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.*;
+
+class Przystanek{
+    private static final LocalDate[] swieta = {
+            LocalDate.of(0,12, 25),
+            LocalDate.of(0, 11, 11)
+    };
+        //tablica rozkładów
+    private static final LocalTime[][] rozklad_normalny = {
+            //linia 1
+            {
+                LocalTime.of(1,2),
+                LocalTime.of(5, 44)
+            },
+            //linia 2
+            {
+                LocalTime.of(1, 34),
+                LocalTime.of(4, 23)
+            }
+    };
+
+}
+
 
 public class Main {
 
     public static void main(String[] args) {
-	    Samochod autko1 = new Samochod("bmw", 420);
-        Samochod autko2 = new Samochod("volvo", 69);
-        LocalDateTime data1 = LocalDateTime.now();
-        DayOfWeek dayofweek = data1.getDayOfWeek();
+        Scanner czytacz = new Scanner(System.in);
 
-        SimpleDateFormat dataformater = new SimpleDateFormat("HH:mm:ss");
+        LocalDateTime dataDzis = LocalDateTime.now();
+        DayOfWeek dayofweek = dataDzis.getDayOfWeek();
 
-        String[] linie = new String[]{"Hehe", "Hehe"};
-        System.out.print(Arrays.toString(linie));
+        if (dayofweek.getValue() >= 6){
+            System.out.println("Weekend");
+            //wypisuje         dzień tygodnia według    styl wypisz wszystko Locale = lokalizacja u nas polska
+            System.out.println(dayofweek.getDisplayName(TextStyle.FULL, Locale.GERMAN));
+        }
+        else{
+            System.out.println("Mamy zwykły dzień tygodnia");
+        }
+
+        DateTimeFormatter formatGodzinowy = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        System.out.println("Wpisz TAK jeśli mam pokazać aktualną godzinę");
+        String command = czytacz.next();
+        if (command.equals("TAK")){
+            System.out.println(dataDzis.format(formatGodzinowy));
+        }
+
+
+
+
+        //przełącznik
+//        switch (command){
+//            case "tak":
+//                System.out.println("Okej działam");
+//                break;
+//            case "może":
+//                // do the stuff
+//                break;
+//            case "nie":
+//                break;
+//            default:
+//                System.out.println("Pierdol sie");
+//        }
+
     }
 }
